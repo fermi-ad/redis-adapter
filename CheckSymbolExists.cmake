7b9a45f3720b3b3cd9e5ba24af25296c2fd7e9a1
Dave Bracey <dbracey@fnal.gov>
2024-01-04 16:05:01 -0600

    get cmake to work in empty container and build

---
+++ CheckSymbolExists.cmake
@@ -0,0 +1,3 @@
+macro(CHECK_SYMBOL_EXISTS _symbol _file _var)
+  set(${_var} TRUE PARENT_SCOPE)
+endmacro()
---
+++ Findhiredis.cmake
@@ -0,0 +1,3 @@
+set(hiredis_FOUND TRUE)
+set(hiredis_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/hiredis)
+set(hiredis_LIBRARIES hiredis)
