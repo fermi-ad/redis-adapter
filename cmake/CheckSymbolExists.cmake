221d358534dedbbac03a648c7ff310ee83b59698
Dave Bracey <dbracey@fnal.gov>
2024-01-05 12:16:29 -0600

    change CheckSymbolExists to match cmake version (but always return 1)

--- cmake/CheckSymbolExists.cmake
+++ cmake/CheckSymbolExists.cmake
@@ -1,5 +1,5 @@
 # override the cmake provided version of this macro
-# always set var to TRUE
+# always set var to 1
 macro(check_symbol_exists _symbol _file _var)
-  set(${_var} TRUE PARENT_SCOPE)
+  set(${_var} 1 CACHE INTERNAL "Have symbol ${_symbol}")
 endmacro()
