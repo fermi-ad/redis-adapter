3de899272597c525b5983cb674ead4fc2a145b6f
Robert Santucci <rsantucc@fnal.gov>
2024-10-11 10:50:42 -0500

    Removed redis.conf in favor of a start script. Defaulted to system /tmp folder insated of this project for test

---
+++ redisTestStart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Default values for Redis server parameters
+BIND_ADDRESS="127.0.0.1"
+APPENDONLY="no"
+SAVE=""
+DAEMONIZE="yes"
+UNIXSOCKET="/tmp/RedisAdapterTest.sock"
+UNIXSOCKETPERM="700"
+
+# Define the path to the Redis configuration file
+REDIS_CONFIG=
+
+# Start the Redis server with the specified command
+redis-server --bind $BIND_ADDRESS --appendonly $APPENDONLY --save "$SAVE" --daemonize $DAEMONIZE --unixsocket $UNIXSOCKET --unixsocketperm $UNIXSOCKETPERM $REDIS_CONFIG
+
+echo "Redis server started"
+
+
