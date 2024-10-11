52163f0e00406b9a1e0fad7787698ea6932f1b31
Bobby Santucci <rsantucc@fnal.gov>
2024-10-11 13:26:11 -0500

    Redis start script (#5)

---
+++ redis-start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Default values for Redis Server
+BIND_ADDRESS="127.0.0.1"
+APPENDONLY="no"
+SAVE=""
+DAEMONIZE="yes"
+UNIXSOCKET="/tmp/redis.sock"
+UNIXSOCKETPERM="700"
+
+# Define the path to the Redis configuration file
+# Default value is empty
+REDIS_CONFIG=
+
+# Start the Redis server with the specified command
+redis-server --bind $BIND_ADDRESS --appendonly $APPENDONLY --save "$SAVE" --daemonize $DAEMONIZE --unixsocket $UNIXSOCKET --unixsocketperm $UNIXSOCKETPERM $REDIS_CONFIG
+
+echo "Redis server started"
+
+
