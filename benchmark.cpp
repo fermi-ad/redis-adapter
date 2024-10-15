ba673ac1857c26845758074a652fd7766ef83e1a
Robert Santucci <rsantucc@fnal.gov>
2024-10-15 09:29:28 -0500

    syntax clean up and updated readme

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,62 +1,58 @@
 #include <benchmark/benchmark.h>
 #include "RedisAdapter.hpp"
-#include <cstdlib>  // For std::getenv
-#include <vector>   // For std::vector
+#include <cstdlib>  
+#include <vector>   
 
 using namespace std;
 using namespace sw::redis;
 
 // Helper function to retrieve the host from the environment variable
-std::string get_redis_host() {
-    const char* env_host = std::getenv("REDIS_ADAPTER_BENCHMARK_HOST");
-    return env_host ? std::string(env_host) : "localhost";  // Default to "localhost" if not set
+std::string get_redis_host() 
+{
+   const char* env_host = std::getenv("REDIS_ADAPTER_BENCHMARK_HOST");
+   return env_host ? std::string(env_host) : "localhost";  // Default to "localhost" if not set
 }
 
 // Helper function to generate a vector of floats of a given size
-std::vector<float> generate_list(size_t size, float value = 1.0f) {
-    return std::vector<float>(size, value);  // Fill the list with the given value
+std::vector<float> generate_list(size_t size, float value = 1.0f) 
+{
+   return std::vector<float>(size, value);  // Fill the list with the given value
 }
 
 // Single value add benchmark
-static void Benchmark_AddSingleValue(benchmark::State& state) {
-    RedisAdapter redis("TEST", { .host = get_redis_host() });
-    for (auto _ : state) {
-        redis.addSingleValue("benchmark_key", "benchmark_value");
-    }
+static void Benchmark_AddSingleValue(benchmark::State& state) 
+{
+   RedisAdapter redis("TEST", { .host = get_redis_host() });
+   for (auto _ : state) { redis.addSingleValue("benchmark_key", "benchmark_value");}
 }
 
 // Single value get benchmark
-static void Benchmark_GetSingleValue(benchmark::State& state) {
+static void Benchmark_GetSingleValue(benchmark::State& state) 
+{
     RedisAdapter redis("TEST", { .host = get_redis_host() });
     redis.addSingleValue("benchmark_key", "benchmark_value");
-    for (auto _ : state) {
-        std::string value;
-        redis.getSingleValue("benchmark_key", value);
-    }
+    for (auto _ : state) { std::string value; redis.getSingleValue("benchmark_key", value);}
 }
 
 // Add list to Redis benchmark with dynamic size
-static void Benchmark_AddList(benchmark::State& state) {
+static void Benchmark_AddList(benchmark::State& state) 
+{
     RedisAdapter redis("TEST", { .host = get_redis_host() });
     size_t size = state.range(0);  // Use the range to define the size of the list
     std::vector<float> values = generate_list(size);
 
-    for (auto _ : state) {
-        redis.addSingleList("benchmark_list_key", values, {.trim = 100});
-    }
+    for (auto _ : state) { redis.addSingleList("benchmark_list_key", values, {.trim = 100});}
 }
 
 // Get list from Redis benchmark with dynamic size
-static void Benchmark_GetList(benchmark::State& state) {
+static void Benchmark_GetList(benchmark::State& state) 
+{
     RedisAdapter redis("TEST", { .host = get_redis_host() });
     size_t size = state.range(0);
     std::vector<float> values = generate_list(size);
     redis.addSingleList("benchmark_list_key", values, {.trim = 100});
 
-    for (auto _ : state) {
-        std::vector<float> result;
-        redis.getSingleList("benchmark_list_key", result);
-    }
+    for (auto _ : state) { std::vector<float> result; redis.getSingleList("benchmark_list_key", result);}
 }
 
 //Add Single Value
@@ -68,12 +64,12 @@ BENCHMARK(Benchmark_GetSingleValue);
 BENCHMARK(Benchmark_AddList)->Arg(256)->Arg(512)->Arg(1024)->Arg(1536)->Arg(2048)->Arg(3072)->Arg(4096)
                             ->Arg(6144)->Arg(8192)->Arg(12288)->Arg(16384)->Arg(24576)->Arg(32768)->Arg(49152)
                             ->Arg(65536)->Arg(131072)->Arg(262144)->Arg(524288)->Arg(1048576)->Arg(2097152)
-                            ->Arg(4194304)->Arg(8388608);  // Up to 8MB
+                            ->Arg(4194304)->Arg(8388608);  // These are vector sizes, total size in bytes will be 4x
 //Get List of different sizes
 BENCHMARK(Benchmark_GetList)->Arg(256)->Arg(512)->Arg(1024)->Arg(1536)->Arg(2048)->Arg(3072)->Arg(4096)
                             ->Arg(6144)->Arg(8192)->Arg(12288)->Arg(16384)->Arg(24576)->Arg(32768)->Arg(49152)
                             ->Arg(65536)->Arg(131072)->Arg(262144)->Arg(524288)->Arg(1048576)->Arg(2097152)
-                            ->Arg(4194304)->Arg(8388608);  // Up to 8MB
+                            ->Arg(4194304)->Arg(8388608);  // These are vector sizes, total size in bytes will be 4x
 
 BENCHMARK_MAIN();
 
