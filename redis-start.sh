65d2895da9b1c40692204784f34e010adec797a9
Dave Bracey <dbracey@fnal.gov>
2025-02-19 09:31:21 -0600

    tweaks test.yml

--- redis-start.sh
+++ redis-start.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Default values for Redis Server
 BIND_ADDRESS="127.0.0.1"
 APPENDONLY="no"
-SAVE=""
+SAVE=
 DAEMONIZE="yes"
 UNIXSOCKET="/tmp/redis.sock"
 UNIXSOCKETPERM="700"
-
-# Define the path to the Redis configuration file
-# Default value is empty
 REDIS_CONFIG=
 
-# Start the Redis server with the specified command
 redis-server --bind $BIND_ADDRESS --appendonly $APPENDONLY --save "$SAVE" --daemonize $DAEMONIZE --unixsocket $UNIXSOCKET --unixsocketperm $UNIXSOCKETPERM $REDIS_CONFIG
 
-echo "Redis server started"
-
-
+if [ $? -eq 0 ]; then
+  echo "redis-server started"
+else
+  echo "redis-server failed to start"
+fi
