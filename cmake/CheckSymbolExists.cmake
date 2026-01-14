c4306283af10ba01a7f65c859a91e5b77b27a4c0
Dave Bracey <dbracey@fnal.gov>
2026-01-14 14:56:34 -0600

    point redis++ at hiredis.h using correct cmake symbol, remove unneeded symbol exists

--- cmake/CheckSymbolExists.cmake
+++
@@ -1,5 +0,0 @@
-# override the cmake provided version of this macro
-# always set var to 1
-macro(check_symbol_exists _symbol _file _var)
-  set(${_var} 1 CACHE INTERNAL "Have symbol ${_symbol}")
-endmacro()
